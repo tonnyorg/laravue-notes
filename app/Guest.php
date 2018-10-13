5b695005d9a70b960b1eaadb1b23303c803ead25
Tonny <dev@tonny.org>
2018-10-12 19:39:07 -0700

    Created model and migration for Guest

---
+++ app/Guest.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Guest extends Model
+{
+    use SoftDeletes;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'token',
+    ];
+
+    /**
+     * The attributes that should be mutated to dates.
+     *
+     * @var array
+     */
+    protected $dates = [
+        'created_at',
+        'deleted_at',
+        'updated_at',
+    ];
+}
