0ec3aaa3086748152187bda34d23d6c49ec6c5a8
Tonny <dev@tonny.org>
2018-10-12 19:10:31 -0700

    Created model and migration for Notes

---
+++ app/Note.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Note extends Model
+{
+    use SoftDeletes;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'content',
+        'style',
+        'title',
+    ];
+
+    /**
+     * The attributes that should be mutated to dates.
+     *
+     * @var array
+     */
+    protected $dates = [
+        'created_at',
+        'deleted_at',
+        'updated_at',
+    ];
+}
