76e267442689725d689efca80483c0c4470c892d
Tonny <dev@tonny.org>
2018-10-13 22:47:07 -0700

    Added guest() relationship and scopeByGuestToken() filter to Note model

--- app/Note.php
+++ app/Note.php
@@ -31,4 +31,26 @@ class Note extends Model
         'deleted_at',
         'updated_at',
     ];
+
+    /**
+     * Get the guest that owns the note.
+     */
+    public function guest()
+    {
+        return $this->belongsTo(Guest::class, 'guest_id', 'id');
+    }
+
+    /**
+     * Scope a query to filter by guest's token.
+     *
+     * @param  \Illuminate\Database\Eloquent\Builder  $query
+     * @param  string   $token
+     * @return \Illuminate\Database\Eloquent\Builder
+     */
+    public function scopeByGuestToken($query, string $token)
+    {
+        return $query->whereHas('guest', function ($query) use ($token) {
+            $query->where('token', $token);
+        });
+    }
 }
