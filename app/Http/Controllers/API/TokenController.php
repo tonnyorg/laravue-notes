d1440d9081168032c0b405523a58e91a15f967ba
Tonny <dev@tonny.org>
2018-10-13 20:34:18 -0700

    Created and implemented TokenController in api.php routes

---
+++ app/Http/Controllers/API/TokenController.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use Illuminate\Http\Request;
+
+use App\Http\Controllers\Controller;
+use App\Guest;
+
+class TokenController extends Controller
+{
+    /**
+     * Validate or generate a new guest token.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function index(Request $request)
+    {
+        $guest = null;
+        $token = preg_replace('/[^a-z0-9]/i', '', $request->cookie('token'));
+
+        if (!empty($token)) {
+            $guest = Guest::byToken($token)->first();
+
+            if ($guest) {
+                return response()->json([
+                        'token' => $guest->token,
+                    ])
+                    ->cookie('token', $guest->token);
+            }
+        }
+
+        $guest = Guest::create([
+            'token' => md5(uniqid()),
+        ]);
+
+        return response()->json([
+                'token' => $guest->token,
+            ])
+            ->cookie('token', $guest->token);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -16,3 +16,7 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::namespace('API')->group(function() {
+    Route::get('token', 'TokenController@index');
+});
