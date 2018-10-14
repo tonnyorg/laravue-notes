1e0a32f9b0943990f6faf14c4327771995b04ba5
Tonny <dev@tonny.org>
2018-10-13 20:32:59 -0700

    Added "scopeByToken to Guest model

--- app/Guest.php
+++ app/Guest.php
@@ -28,4 +28,16 @@ class Guest extends Model
         'deleted_at',
         'updated_at',
     ];
+
+    /**
+     * Scope a query to filter by token.
+     *
+     * @param  \Illuminate\Database\Eloquent\Builder  $query
+     * @param  string   $token
+     * @return \Illuminate\Database\Eloquent\Builder
+     */
+    public function scopeByToken($query, string $token)
+    {
+        return $query->where('token', $token);
+    }
 }
