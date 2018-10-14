04f53df32c6663170286a691086a33bfd634c0af
Tonny <dev@tonny.org>
2018-10-13 20:59:23 -0700

    Created VerifyGuestToken middleware

---
+++ app/Http/Middleware/VerifyGuestToken.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+
+use App\Guest;
+
+class VerifyGuestToken
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        $token = filterTokenValue($request->cookie('token', ''));
+        $guest = Guest::byToken($token)->first();
+
+        if (!$guest) {
+            return response()->json([
+                'errors' => [],
+                'message' => 'Unauthorized access',
+            ], 401);
+        }
+
+        return $next($request);
+    }
+}
